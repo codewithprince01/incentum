bfee14efcded40653604b4e2d950945017311e85
Harshit Saini <harshitsaini0019@gmail.com>
2024-12-03 16:05:12 +0530

    tone

---
+++ src/components/form/Step.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+export default function Step() {
+  return (
+    <div className="flex justify-between items-center mb-6">
+          {[1, 2, 3, 4].map((step, index) => (
+            <div key={step} className="flex items-center">
+              <div
+                className={`w-12 h-12 bg-blue-500 text-white flex items-center justify-center rounded-lg border-4 ${
+                  index <= 2 ? 'border-blue-400' : 'border-blue-200'
+                }`}
+              >
+                {step}
+              </div>
+              {index !== 3 && (
+                <div className="w-16 h-1 bg-blue-400 rounded-full mx-2"></div>
+              )}
+            </div>
+          ))}
+        </div>
+  )
+}
+
